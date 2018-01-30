2d343773163970405d138dda0270d2ca9682913e
birju.vachhani <brvachhani@gmail.com>
2018-01-30 19:32:24 +0530

    Some changes in UI Add Rotate Animation on arrow button

--- app/src/main/java/com/birjuvachhani/expandablerecyclerviewexample/RecyclerAdapter.java
+++ app/src/main/java/com/birjuvachhani/expandablerecyclerviewexample/RecyclerAdapter.java
@@ -1,6 +1,7 @@
 package com.birjuvachhani.expandablerecyclerviewexample;
 
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -12,6 +13,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -62,27 +66,29 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Recycl
         TextView mtitle;
         TextView mdesc;
         Context context;
-        LinearLayout container;
+        LinearLayout container, child;
         ImageView arrow;
 
-        public RecyclerViewHolder(final Context context, View itemView) {
+        public RecyclerViewHolder(final Context context, final View itemView) {
             super(itemView);
             this.context = context;
             mimg = (ImageView) itemView.findViewById(R.id.iv_image);
             mtitle = (TextView) itemView.findViewById(R.id.tv_title);
             mdesc = (TextView) itemView.findViewById(R.id.tv_desc);
             container = (LinearLayout) itemView.findViewById(R.id.container);
-            arrow=(ImageView)itemView.findViewById(R.id.arrow);
+            arrow = (ImageView) itemView.findViewById(R.id.arrow);
+            child = (LinearLayout) itemView.findViewById(R.id.child);
 
             arrow.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (mdesc.getVisibility() == View.GONE) {
-                        mdesc.setVisibility(View.VISIBLE);
-                        arrow.setImageResource(R.drawable.ic_keyboard_arrow_up_white_36dp);
+                    if (child.getVisibility() == View.GONE) {
+                        ObjectAnimator.ofFloat(arrow, "rotation", 0, 180).start();
+                        child.setVisibility(View.VISIBLE);
 
                     } else {
-                        mdesc.setVisibility(View.GONE);
+                        child.setVisibility(View.GONE);
+                        ObjectAnimator.ofFloat(arrow, "rotation", 180, 0).start();
                         arrow.setImageResource(R.drawable.ic_keyboard_arrow_down_white_36dp);
                     }
                 }
