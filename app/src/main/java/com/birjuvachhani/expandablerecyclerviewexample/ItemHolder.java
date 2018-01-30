2e7581d90a3540b1c9258350b95db4f3036e42d8
birju.vachhani <brvachhani@gmail.com>
2018-01-30 17:30:48 +0530

    Add Background color in ItemHolder POJO
    Remove Expandable RecyclerView Dependency

--- app/src/main/java/com/birjuvachhani/expandablerecyclerviewexample/ItemHolder.java
+++ app/src/main/java/com/birjuvachhani/expandablerecyclerviewexample/ItemHolder.java
@@ -1,5 +1,13 @@
 package com.birjuvachhani.expandablerecyclerviewexample;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.Log;
+
+import com.thoughtbot.expandablerecyclerview.models.ExpandableGroup;
+
+import java.util.List;
+
 /**
  * Created by birju.vachhani on 30/01/18.
  */
@@ -9,11 +17,13 @@ public class ItemHolder {
     private String title;
     private String desc;
     private int ImageId;
+    private int backgroundColor;
 
-    public ItemHolder(String title, String desc, int imageId) {
+    public ItemHolder(String title, String desc, int imageId, int backgroundColor) {
         this.title = title;
         this.desc = desc;
         ImageId = imageId;
+        this.backgroundColor=backgroundColor;
     }
 
     public String getTitle() {
@@ -26,5 +36,9 @@ public class ItemHolder {
 
     public int getImageId() {
         return ImageId;
+    }
+
+    public int getBackgroundColor() {
+        return backgroundColor;
     }
 }
